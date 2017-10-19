69b8a6871e01ca7a8f97c577d39fa7f7b3726569
bogdangoz78 <bogdan78@wp.pl>
2017-10-19 16:06:16 +0200

    pierwszy program zakonczony

---
+++ program.c
@@ -0,0 +1,16 @@
+
+/*dolacz biblioteke standardowa input-output*/
+#include <stdio.h>
+
+/* y = f(x) */
+/*	 0= main() */
+
+int main() /*funkcja glowna programu , zwraca wartosc całkowita*/
+{/* między klamrami jest ciało funkcji*/
+
+		puts("Pierwszy program");/* funkcja z biblioteki studio */
+	return 0; /* main zwraca wartosc 0*/
+}
+
+	
+
